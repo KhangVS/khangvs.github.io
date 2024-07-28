8951ba5f89108e750b1c2cb3634a30a7e72aea78
tbang188 <thanhbang112000@gmail.com>
2024-07-28 21:50:25 +0700

    Update Send Mail Section

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -16,3 +16,40 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.getElementById("send-email").addEventListener('click', (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    let content = document.getElementById("message").value;
+    if (name && email && content) {
+        let data = {
+            name: name,
+            email: email,
+            content: content,
+        }
+        postGoogle(data);
+        alert("Email sent successfully!");
+        document.getElementById("contact-form").reset();
+    }
+});
+
+async function postGoogle(data) {
+    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSf4ZD_-Nn90mdQeljFi17PpxFAtDM2-WDujajuiCJDcabZkxQ/formResponse";
+    const PostName = "entry.1082309334";
+    const PostEmail = "entry.764353114";
+    const PostContent = "entry.719523195";
+    const formData = new FormData();
+    formData.append(PostName, data.name);
+    formData.append(PostEmail, data.email);
+    formData.append(PostContent, data.content);
+    try {
+        await fetch(formURL, {
+            method: "POST",
+            body: formData,
+        })
+    }
+    catch (error) {
+        console.error("Error sending form data:", error);
+    }
+}
